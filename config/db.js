3dbd1d29d495631356564ad08a2b0d067925b99e
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-07 15:13:57 +0100

    adds try,catch for db connection

--- config/db.js
+++ config/db.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-  console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    console.log(
+      `MongoDB connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
