514214a5cede99386f10a531c08e99b5e4af6aeb
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-08 15:25:13 +0100

    fixing error with date in User model, creating new user, hashing password with bcryptjs, saving user to db

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
@@ -20,13 +21,40 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // creating new user
+      user = new User({
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      // create salt with 10 rounds
+      const salt = await bcrypt.genSalt(10);
+      // hashed version of the password
+      user.password = await bcrypt.hash(password, salt);
+      // save user to db
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
