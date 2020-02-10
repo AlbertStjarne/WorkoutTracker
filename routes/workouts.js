65ef19f4ba5bd708abeb4338490454b2b0cbc011
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-10 15:20:39 +0100

    adds auth and getting workouts for a user

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
+
+const User = require('../models/User');
+const Workout = require('../models/Workout');
 
 // @route   GET api/workouts
 // @desc    Get all workouts for a user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all workouts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const workouts = await Workout.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(workouts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/workouts
