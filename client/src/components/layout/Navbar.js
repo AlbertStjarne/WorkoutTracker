1bc571a528cfd494a45391f333bcf692ba542c2a
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-12 16:47:08 +0100

    adds components with layout folder, adds Navbar component, installing react-router-dom, adds pages folder with start of Home and About components, adds routes for Home and About, adds Link in Navbar to Home and About

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1 className={icon}>{title}</h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Workout Tracker',
+  icon: 'fas fa-dumbbell',
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Workout Tracker</h1>
+      <p className='my-1'>Are you ready to step up your workout game?</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
