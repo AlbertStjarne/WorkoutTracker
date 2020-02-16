dfd319a94fba589157ec75248ccbe57909681f2f
Albert Stjärne <albert.stjarne@gmail.com>
2020-02-16 18:38:00 +0100

    adds context/alert folder with context, reducer and state files, adds alertReducer with SET_ and REMOVE_ALERT cases, adds AlertState outputting AlertContext, adds Alerts component outputting the alert message, importing and wrapping with AlertState and outputting Alerts

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,3 +6,5 @@ export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 export const FILTER_WORKOUTS = 'FILTER_WORKOUTS';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
